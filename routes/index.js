e5b461da72fd46acafb1c3b8717e656077cb13f7
Jaylyn Stoesz <jaylynstoesz@gmail.com>
2015-09-28 17:59:15 -0600

    project information added

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var router = express.Router()
+var lib = require('../lib/projects')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +9,11 @@ router.get('/', function(req, res, next) {
 
 router.get('/about', function (req, res, next) {
   res.render('about', { title: 'Jaylyn Stoesz' })
+})
+
+router.get('/projects/:projectId', function (req, res, next) {
+  var project = lib.projects[req.params.projectId]
+  res.render('project', { title: 'Jaylyn Stoesz', project: project })
 })
 
 module.exports = router;
